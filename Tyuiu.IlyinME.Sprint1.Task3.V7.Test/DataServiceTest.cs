65727c0868d7848aa0dcf008baa59604c07a7806
Tyuiu.IlyinME@outlook.com <maxchance05@mail.ru>
2023-09-22 21:20:48 +0500

    Task3

---
+++ Tyuiu.IlyinME.Sprint1.Task3.V7.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.IlyinME.Sprint1.Task3.V7.Lib;
+
+namespace Tyuiu.IlyinME.Sprint1.Task3.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double verst = 100;
+            var res = ds.VerstsToKilometers(verst);
+            Assert.AreEqual(106.68, res);
+        }
+    }
+}
