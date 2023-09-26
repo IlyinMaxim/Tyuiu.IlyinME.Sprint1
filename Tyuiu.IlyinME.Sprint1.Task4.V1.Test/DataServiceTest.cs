513ea082f094d19ce886781e1b108e77a1d6dba3
Tyuiu.IlyinME@outlook.com <maxchance05@mail.ru>
2023-09-26 19:30:28 +0500

    Task4

---
+++ Tyuiu.IlyinME.Sprint1.Task4.V1.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.IlyinME.Sprint1.Task4.V1.Lib;
+
+namespace Tyuiu.IlyinME.Sprint1.Task4.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 4;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(0.028, res);
+        }
+    }
+}
---
+++ Tyuiu.IlyinME.Sprint1.Task4.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IlyinME.Sprint1.Task4.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IlyinME.Sprint1.Task4.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("42d7199d-a54f-4c85-85ad-e60c0ea5aad2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
